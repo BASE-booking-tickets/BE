07c094691096f9ddffb222c7872b060ff6af51b2
Tuấn Khanh <tuankhanh111904@gmail.com>
2026-01-13 15:57:09 +0700

    first commit: initial project setup (Node.js backend)

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import connectDB from "./src/configs/connectDB.js";
+import notFoundRequest from "./src/middlewares/notFoundRequest.js";
+import { HOST, PORT } from "./src/configs/dotenvConfig.js";
+import router from "./src/routes/index.js";
+import dotenv from "dotenv"
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.use("/", router);
+
+// * handle Notfound Request
+app.use(notFoundRequest);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${HOST}:${PORT}`);
+});
