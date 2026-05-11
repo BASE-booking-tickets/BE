be92752fc4d98f580636e0524bfd4e80d706b6cb
Tuấn Khanh <tuankhanh111904@gmail.com>
2026-05-11 10:53:51 +0700

    fix logic

--- src/shared/configs/vnpay.configs.js
+++ src/shared/configs/vnpay.configs.js
@@ -1,7 +1,7 @@
 // config/vnpay.config.js
 export const vnpayConfig = {
-    vnp_TmnCode: "KTKN8AEF",
-    vnp_HashSecret: "MTYYW66SNT2Z0IBFD8YP2CP2V0SFMG5H",
+    vnp_TmnCode: "AT29O5VD",
+    vnp_HashSecret: "ISAP8D8AC21QXZHXDD53PHHK95XWW82U",
     vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
     vnp_ReturnUrl: "http://localhost:5173/payment-result",
     vnp_Api: "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
